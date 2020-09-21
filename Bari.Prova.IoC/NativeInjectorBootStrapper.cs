2292e3bb6e2d81a14e7aa2472dfdb688f7ee2fc2
eltonpalini <71649788+eltonpalini@users.noreply.github.com>
2020-09-21 11:05:10 -0300

    Add files via upload

---
+++ Bari.Prova.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Bari.Prova.Application;
+using Bari.Prova.Application.Interface;
+using Bari.Prova.Application.Service;
+
+namespace Bari.Prova.IoC
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IMessageService, MessageService>();
+        }
+    }
+}
