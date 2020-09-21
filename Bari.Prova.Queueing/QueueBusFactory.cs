bceefa0cb9eafbd6aacf5ee1018d3db94ff6281b
eltonpalini <71649788+eltonpalini@users.noreply.github.com>
2020-09-21 11:19:02 -0300

    Add files via upload

---
+++ Bari.Prova.Queueing/QueueBusFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bari.Prova.Queueing
+{
+    public class QueueBusFactory
+    {
+        public static IQueueBus Create(string address, string userName, string password)
+        {
+            //TODO: Implementar outros tipos de QueueBus
+            return new Rabbit.QueueBusRabbit(address, userName, password);
+        }
+    }
+}
