3f62a92082d5259215ccb2a874113d0ffd7b403c
eltonpalini <71649788+eltonpalini@users.noreply.github.com>
2020-09-21 11:03:16 -0300

    Add files via upload

---
+++ Bari.Prova.Application/Interface/IMessageService.cs
@@ -0,0 +1,15 @@
+﻿using Bari.Prova.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bari.Prova.Application.Interface
+{
+    public interface IMessageService
+    {
+        void SendMessage(Message message);
+        Message ReceiveMessage();
+        void StartListener();
+        event EventHandler Listener;
+    }
+}
