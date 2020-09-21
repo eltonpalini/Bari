40b0f5ddef9c9db4ed487b40887de169b932b401
eltonpalini <71649788+eltonpalini@users.noreply.github.com>
2020-09-21 11:07:52 -0300

    Add files via upload

---
+++ Bari.Prova.Queueing/IQueueBus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bari.Prova.Queueing
+{
+    public interface IQueueBus : IDisposable
+    {
+        void Publish<T>(T obj);
+        T ReceiveMessage<T>();
+        event EventHandler Listener;
+        string QueueName { get; set; }
+        string Exchange { get; set; }
+        void StartListener();
+        new void Dispose();
+        void RejectMessage(ulong deliveryTag, bool requeue);
+    }
+}
