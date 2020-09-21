10cbb1224626c8457518a68f5f6cc55f8b0c3ece
eltonpalini <71649788+eltonpalini@users.noreply.github.com>
2020-09-21 11:04:09 -0300

    Add files via upload

---
+++ Bari.Prova.Domain/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bari.Prova.Domain
+{
+    public class Message
+    {
+        public Message() { }
+        public Message(string appId, DateTime timeStamp, string id, string text)
+        {
+            AppId = appId;
+            TimeStamp = timeStamp;
+            Id = id;
+            Text = text;
+        }
+
+        public string AppId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Id { get; set; }
+        public string Text { get; set; }
+    }
+}
